dfc7d7ed7bd20b0abbce8cc5ccfcaadd3d0b94dc
Seohyun Lee <studioseorii@gmail.com>
2023-01-17 14:32:25 +0900

    fix: apply nunui design

--- src/lib/Env/TestEnv.svelte
+++ src/lib/Env/TestEnv.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import {ThemeProvider} from 'nunui';
     import {setContext} from 'svelte';
     import api from "$lib/utils/api";
     import throttle from "$lib/utils/throttle";
@@ -15,16 +16,20 @@
     });
 </script>
 
-<main>
-    <nav>
-        <Box blur background="radial-gradient(circle at 0 0, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4) 128%)"
-             style="margin-bottom: 10px; padding: 1.5rem 1.875rem 1.875rem 1.875rem;">
-            <slot/>
-        </Box>
-    </nav>
-</main>
+<ThemeProvider primary="#de5095" onPrimary="#000000" onSurface="#ffffff" surface="#000000">
+    <main>
+        <nav>
+            <Box blur background="radial-gradient(circle at 0 0, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4) 128%)"
+                 style="margin-bottom: 10px; padding: 1.5rem 1.875rem 1.875rem 1.875rem;">
+                <slot/>
+            </Box>
+        </nav>
+    </main>
+</ThemeProvider>
 
 <style lang="scss">
+  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+
   main {
     position: fixed;
     top: 0;
@@ -36,6 +41,9 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    font: normal 16px 'Spoqa Han Sans Neo', 'sans-serif';
+    background: black;
+    color: white;
 
     nav {
       width: 21.8%;
@@ -43,6 +51,11 @@
       min-width: 23.75rem;
       max-height: 88%;
       overflow-y: auto;
+      scroll-behavior: smooth;
+
+      * {
+        -webkit-tap-highlight-color: transparent;
+      }
     }
   }
 </style>
