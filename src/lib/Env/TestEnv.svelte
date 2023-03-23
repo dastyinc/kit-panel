c1451d718d784783a9d615cd623a4a7312db475d
wagyu1001 <1001hermes@gmail.com>
2023-03-23 21:40:32 +0900

    fix: box ui

--- src/lib/Env/TestEnv.svelte
+++ src/lib/Env/TestEnv.svelte
@@ -13,8 +13,7 @@
     </span>
     <main>
         <nav>
-            <Box blur background="radial-gradient(circle at 0 0, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4) 128%)" expand
-                 style="margin-bottom: 10px; padding: 1.5rem 1.875rem 1.875rem 1.875rem;">
+            <Box background="background-color: var(--Main_Black, #1c1c1e);" expand style="margin-bottom: 10px;">
                 {#key channel}
                     <TestEnvInner {channel} {account}>
                         <slot/>
